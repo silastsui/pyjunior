6aab549f258e101000460044a73df3b0da2e1fe6
silastsui <silastsuii@gmail.com>
2016-02-23 11:20:13 -0800

    Update and rename virus.py to intloops/virus.py

--- intloops/virus.py
+++ intloops/virus.py
@@ -7,13 +7,3 @@ while infected < 7300000000:
 	infected = 2**day + infected
 	day = day + 1
 print "The world will end in", day, "days"
-	
-
-	
-#1
-#2 + 6
-#9
-#33
-#123
-#add total infected, multiply by 2 = infected people for the day
-#sum total infected
