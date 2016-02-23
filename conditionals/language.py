1ba8e2f6a20694175131685efcbc775142bb0879
silastsui <silastsuii@gmail.com>
2016-02-23 11:10:58 -0800

    Added files via upload

---
+++ conditionals/language.py
@@ -0,0 +1,18 @@
+hello = raw_input("what language do you speak? ")
+
+if hello == 'german':
+	print 'gutentag'
+elif hello == 'chinese':
+	print 'ni hao'
+elif hello == 'italian':
+	print 'ciao'
+elif hello == 'spanish':
+	print 'hola'
+elif hello == 'klingon':
+	print 'nuqneH'
+elif hello == 'english':
+	print 'hello'
+else:
+	print 'sorry, i don\'t recognize your language'
+print 'goodbye'
+	
